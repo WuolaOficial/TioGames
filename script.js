6b0218820ede7aa755980fba09d8e1f18706f2c1
WuolaOficial <wuolacuenta@gmail.com>
2024-02-27 02:39:23 +0100

    nueva seccion

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function search() {
-    /* Crea las variables */
+    
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('searchInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("dataList");
     li = ul.getElementsByTagName('li');
   
-    // Loop through all list items, and hide those who don't match the search query
+    
     for (i = 0; i < li.length; i++) {
       txtValue = li[i].textContent || li[i].innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -15,7 +15,7 @@ function search() {
         li[i].style.display = "none";
       }
     }
-  }// Verificar si la tecla presionada es "Enter"// Ejecutar la búsqueda si es "Enter"
+  }
   
   function handleKeyPress(event) {
     
@@ -29,11 +29,11 @@ function search() {
     ul = document.getElementById("dataList");
     li = ul.getElementsByTagName('li');
   
-    // Mostrar todos los elementos
+    
     for (i = 0; i < li.length; i++) {
       li[i].style.display = "";
     }
   
-    // Limpiar el campo de búsqueda
+    
     document.getElementById('searchInput').value = "";
   }
